8135097b2017f81f79960ef592f6df8a72ed55d0
eric <erichuan2020@gmail.com>
2018-12-25 19:08:25 -0500

    add id to redux store reducer

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,14 +1,17 @@
+import { SIGN_IN, SIGN_OUT } from "../actions/types";
+
 const INITIAL = {
-  isSignedIn: null
+  isSignedIn: null,
+  userId: null
 };
 
 export default (state = INITIAL, action) => {
   switch (action.type) {
-    case "SIGN_IN":
-      return { ...state, isSignedIn: true };
+    case SIGN_IN:
+      return { ...state, isSignedIn: true, userId: action.payload };
       break;
-    case "SIGN_OUT":
-      return { ...state, isSignedIn: false };
+    case SIGN_OUT:
+      return { ...state, isSignedIn: false, userId: null };
       break;
     default:
       return state;
