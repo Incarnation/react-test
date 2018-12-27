b0ddac16349e46422e055113700f112a4c541597
eric <erichuan2020@gmail.com>
2018-12-27 15:47:58 -0500

    add streaming video, add flv player

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,13 +1,52 @@
 import React from "react";
+import flv from "flv.js";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
 
 class StreamShow extends React.Component {
+  //constructor
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
+  }
+
+  //function call when the component gets render first time
   componentDidMount() {
     //console.log(this.props);
     this.props.fetchStream(this.props.match.params.id);
+
+    this.buildPlayer();
   }
 
+  //after the component render the second time
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  //life cycle method when user leave the page
+  //clean up the resources when user leave the page
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  //helper method to create the flv player
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    const id = this.props.match.params.id;
+    //add the player to the screen using the flv library
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+  }
+
+  //render the show stream component
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>;
@@ -15,6 +54,7 @@ class StreamShow extends React.Component {
 
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls={true} />
         <h1>{this.props.stream.title}</h1>
         <h5>{this.props.stream.description}</h5>
       </div>
